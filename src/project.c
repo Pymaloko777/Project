a26aa09a80d26e52a9184d198cd85477407c7aa5
Misha Belashov <cabaka-babaka@mail.ru>
2017-05-29 23:21:18 +0800

    Added menu and operations

--- src/project.c
+++ src/project.c
@@ -12,5 +12,42 @@ int main()
 	float x,y; 
 	int Dei, Dei1, exit1; 
 	printf ("Menu\n1.One number\n2.Two numbers\n3.Exit\nSelect the action and press Enter\n"); 
+	scanf ("%d", &Dei);
+	switch (Dei) 
+	{ 
+		case 1: 
+		{ 
+			printf ("\n1.Sinus\n2.Cosinus\n3.Square root\n4.Squared\n5.Cubed\n6.Factorial\nSelect the action and press Enter\n"); 
+			scanf ("%d", &Dei1); 
+			switch(Dei1) 
+			{ 
+				case 1: 
+				{ 
+					
+				} 
+				case 2: 
+				{ 
+					
+				} 
+				case 3: 
+				{ 
+					 
+				} 
+				case 4: 
+				{ 
+					
+				} 
+				case 5: 
+				{ 
+					 
+				} 
+				case 6: 
+				{ 
+					 
+				}
+			}
+			break;
+		}
+		
 	return 0;
 }
