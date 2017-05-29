86aefebf46f69eca6dba577c7a7b05f8b91c9e45
Misha Belashov <cabaka-babaka@mail.ru>
2017-05-29 22:53:02 +0800

    Added functions Product and Divit

--- src/func.c
+++ src/func.c
@@ -10,3 +10,15 @@ float Minus (float x, float y)
 	Min=x-y;
 	return Min;
 }
+float Product (float x, float y)
+{
+	float Prod;
+	Prod=x*y;
+	return Prod;
+}
+float Divit (float x, float y)
+{
+	float Div;
+	Div=x/y;
+	return Div;
+}
--- src/func.h
+++ src/func.h
@@ -1,2 +1,4 @@
 float Summa (float x, float y);
 float Minus (float x, float y);
+float Product (float x, float y);
+float Divit (float x, float y);
