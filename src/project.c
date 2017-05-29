782d98a0688de130f50f05906b619eed6c20be36
Misha Belashov <cabaka-babaka@mail.ru>
2017-05-29 23:26:41 +0800

    Added calculations

--- src/project.c
+++ src/project.c
@@ -23,27 +23,51 @@ int main()
 			{ 
 				case 1: 
 				{ 
-					
+					printf ("Enter the number: "); 
+					scanf ("%f", &x); 
+					y=sin(x); 
+					printf ("sin(%0.3f) = %0.3f\n", x,y); 
+					break; 
 				} 
 				case 2: 
 				{ 
-					
+					printf ("Enter the number: "); 
+					scanf ("%f", &x); 
+					y=cos(x); 
+					printf ("cos(%0.3f) = %0.3f\n", x,y); 
+					break;	
 				} 
 				case 3: 
 				{ 
-					 
+					printf ("Enter the number: "); 
+					scanf ("%f", &x); 
+					y=sqrt(x); 
+					printf ("%0.3f ^ (1/2) = %0.3f\n", x,y); 
+					break;  
 				} 
 				case 4: 
 				{ 
-					
+					printf ("Enter the number: "); 
+					scanf ("%f", &x); 
+					y=pow(x,2); 
+					printf ("%0.3f ^ 2 = %0.3f\n", x,y); 
+					break; 	
 				} 
 				case 5: 
-				{ 
-					 
+				{	 
+					printf ("Enter the number: "); 
+					scanf ("%f", &x); 
+					y=pow(x,3); 
+					printf ("%0.3f ^ 3 = %0.3f\n", x,y); 
+					break; 
 				} 
 				case 6: 
 				{ 
-					 
+					printf ("Enter the number: "); 
+					scanf ("%f", &x); 
+					y=Fact(x); 
+					printf ("%0.3f! = %0.3f\n", x,y); 
+					break; 	 
 				}
 			}
 			break;
