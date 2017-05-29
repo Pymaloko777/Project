a78de37710b4b994f3c06d8874d6c1e313d3c9a9
Misha Belashov <cabaka-babaka@mail.ru>
2017-05-29 22:52:29 +0800

    Added functions Minus and Summa

--- src/func.c
+++ src/func.c
@@ -1,1 +1,12 @@
-
+float Summa (float x, float y)
+{
+	float Sum;
+	Sum=x+y;
+	return Sum;
+}
+float Minus (float x, float y)
+{
+	float Min;
+	Min=x-y;
+	return Min;
+}
--- src/func.h
+++ src/func.h
@@ -1,1 +1,2 @@
-
+float Summa (float x, float y);
+float Minus (float x, float y);
